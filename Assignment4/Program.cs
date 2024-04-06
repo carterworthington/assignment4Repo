4232fa4008692013bf522b5dd87d8a145e709263
carterworthington <carterwmusic@gmail.com>
2024-04-06 12:04:59 -0600

    starting to set up Client

---
+++ Assignment4/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Client
+{
+
+
+    //Greedy Constructor
+public Client(string FirstName, string LastName, int Weight, int Height)
+{
+    FirstName=firstname;
+    LastName=lastName;
+    Weight=weight;
+    Height=Height;
+}
+
+
+
+
+
+public string FirstName
+		{
+			get { return _firstname; }
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+					throw new ArgumentNullException("Name is required. Must not be empty or blank.");
+				_name = value;
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+}
